9e1e88d8e3e8227f31271283a08241ca05bb8edc
Samba <junkipark430@gmail.com>
2021-10-06 00:38:47 +0900

    feat: 사용자 입력 기능 중 새로 시작, 종료 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -7,6 +7,8 @@ import nextstep.utils.Console;
 import java.util.List;
 
 public class InputView {
+    private static final String RESTART_NUM = "1";
+    private static final String EXIT_NUM = "2";
     public static BaseballNumber input() {
         System.out.print("숫자를 입력해주세요 : ");
         try {
@@ -16,5 +18,15 @@ public class InputView {
             System.out.println(e.getMessage());
             return input();
         }
+    }
+
+    public static boolean restartInput() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine();
+        if (!(RESTART_NUM.equals(input) || EXIT_NUM.equals(input))) {
+            System.out.println("[ERROR] 1 또는 2가 아닙니다.");
+            return restartInput();
+        }
+        return RESTART_NUM.equals(input);
     }
 }
