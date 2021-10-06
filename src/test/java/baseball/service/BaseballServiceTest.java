2913ddfc3c7da5096e1aaef37be60ae8594bf949
Samba <junkipark430@gmail.com>
2021-10-06 20:15:53 +0900

    test: 야구게임 play 기능 테스트

--- src/test/java/baseball/service/BaseballServiceTest.java
+++ src/test/java/baseball/service/BaseballServiceTest.java
@@ -1,22 +1,27 @@
 package baseball.service;
 
-import org.junit.jupiter.api.BeforeAll;
+import baseball.domain.BaseballNumber;
+import baseball.domain.BaseballResultVO;
+import baseball.view.InputView;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class BaseballServiceTest {
 
-    private static BaseballService baseballService;
-
-    @BeforeAll
-    static void setUp() {
-        baseballService = new BaseballService();
-    }
+    @Spy
+    @InjectMocks
+    private BaseballService baseballService;
 
     @Test
     void 중복없는_3자리수_생성() {
@@ -29,5 +34,20 @@ class BaseballServiceTest {
 
         //then
         assertThat(nonDuplicateNumbers.size()).isEqualTo(numberSize);
+    }
+
+    @Test
+    void 게임_플레이_정상() {
+        //given
+        BaseballNumber userNumbers = new BaseballNumber(Arrays.asList(1, 2, 3));
+        BaseballResultVO result = new BaseballResultVO(0, 0);
+        MockedStatic<InputView> inputView = Mockito.mockStatic(InputView.class);
+
+        //when
+        when(baseballService.createNonDuplicateNumbers()).thenReturn(Arrays.asList(1, 2, 3));
+        inputView.when(() -> InputView.input()).thenReturn(userNumbers);
+
+        //then
+        assertThatCode(() -> baseballService.play()).doesNotThrowAnyException();
     }
 }
