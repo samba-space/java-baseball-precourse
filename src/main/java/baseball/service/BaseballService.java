44ef3f15896b42575e7d3157a6965400d3d498f4
Samba <junkipark430@gmail.com>
2021-10-06 16:03:45 +0900

    feat: 야구게임 play 기능 구현

--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -1,5 +1,9 @@
 package baseball.service;
 
+import baseball.domain.BaseballNumber;
+import baseball.domain.BaseballResultVO;
+import baseball.view.InputView;
+import baseball.view.OutputView;
 import nextstep.utils.Randoms;
 
 import java.util.*;
@@ -8,6 +12,20 @@ public class BaseballService {
     private static final int RANGE_START = 1;
     private static final int RANGE_END = 9;
     private static final int NUMBER_SIZE = 3;
+
+    public void play() {
+        BaseballNumber computerNumbers = new BaseballNumber(createNonDuplicateNumbers());
+        BaseballResultVO result;
+        do {
+            BaseballNumber userNumbers = InputView.input();
+            result = computerNumbers.matchBaseballNumber(userNumbers);
+            OutputView.output(result);
+        } while (!result.isFinish());
+    }
+
+    public boolean isRestart() {
+        return InputView.restartInput();
+    }
 
     public List<Integer> createNonDuplicateNumbers() {
         Set<Integer> nonDuplicateNumbers = new LinkedHashSet<>();
