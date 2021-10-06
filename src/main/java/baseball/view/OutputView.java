10db2abb1ba30656e00f2667628c73c2ed41ac16
Samba <junkipark430@gmail.com>
2021-10-06 20:16:40 +0900

    refactor: 결과 출력 기능 직관적으로 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,24 +4,20 @@ import baseball.domain.BaseballResultVO;
 
 public class OutputView {
     public static void output(BaseballResultVO result) {
-        int strike = result.getStrike();
-        int ball = result.getBall();
-        if (strike > 0) {
-            System.out.printf("%d스트라이크 ", strike);
+        System.out.println(getPrintFormat(result));
+        if (result.isFinish()) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 끝");
         }
-        if (ball > 0) {
-            System.out.printf("%d볼", ball);
-        }
-        printOrderCase(result);
-        System.out.println();
     }
 
-    private static void printOrderCase(BaseballResultVO result) {
-        if (result.isFinish()) {
-            System.out.print("\n3개의 숫자를 모두 맞히셨습니다! 게임 끝");
-        }
+    private static String getPrintFormat(BaseballResultVO result) {
+        String printFormat = String.format("%d스트라이크 %d볼", result.getStrike(), result.getBall());
         if (result.isNoting()) {
-            System.out.print("낫싱");
+            return "낫싱";
         }
+        if (result.isBoth()) {
+            return printFormat;
+        }
+        return  result.getStrike() > 0 ? printFormat.split(" ")[0] : printFormat.split(" ")[1];
     }
 }
